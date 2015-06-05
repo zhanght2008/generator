8de5f928cc845b3f932d368ca9c60901c0e815b9
zhanght2008 <zhanght2008@126.com>
2015-06-05 16:05:35 +0800

    找到生成代码位置  创建codegen/mybatis3/service/ServiceJavaGenerator

---
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/codegen/mybatis3/service/ServiceJavaGenerator.java
@@ -0,0 +1,23 @@
+package org.mybatis.generator.codegen.mybatis3.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.dom.java.CompilationUnit;
+import org.mybatis.generator.codegen.AbstractJavaGenerator;
+
+public class ServiceJavaGenerator extends AbstractJavaGenerator {
+
+	public ServiceJavaGenerator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<CompilationUnit> getCompilationUnits() {
+		List<CompilationUnit> answer = new ArrayList<CompilationUnit>();
+		return answer;
+	}
+
+	
+
+}
